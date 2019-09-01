047a422335d83c33f0cab6a80f8cc4d14189af85
matemik <44001164+matemik@users.noreply.github.com>
2019-09-01 12:59:58 +0200

    editing base template, adding requirements

--- blogapp/views.py
+++ blogapp/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'title': 'First post',
+        'author': 'Matej',
+        'date_posted': '30.08.2019',
+        'content': 'This is the first blog post'
+    },
+    {
+        'title': 'Second post',
+        'author': 'Matej',
+        'date_posted': '30.08.2019',
+        'content': 'This is the second blog post'
+    }
+]
+
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return render(request, 'blog/about.html')
+    return render(request, 'blog/about.html', {'title': 'About'})
